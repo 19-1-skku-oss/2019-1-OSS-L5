172264bca441ba5feadc8fb9e979c53a24b22926
minSW <minsw08@gmail.com>
2019-05-30 15:35:07 +0900

    Apply the simple small logo on top

--- assets/js/src/scripts.js
+++ assets/js/src/scripts.js
@@ -6,12 +6,12 @@ $(function() {
   $(".content").fitVids();
 });
 
-// Projects Menu
-$('.projects-menu-icon').click(function() {
-  $('html').toggleClass('no-scroll');
-  $(this).toggleClass('active');
-  $('.overlay').toggleClass('show');
-});
+// // Projects Menu
+// $('.projects-menu-icon').click(function() {
+//   $('html').toggleClass('no-scroll');
+//   $(this).toggleClass('active');
+//   $('.overlay').toggleClass('show');
+// });
   
 // All others
 $(document).ready(function() {
