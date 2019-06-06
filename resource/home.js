5cb6aa80307af85be41c9741c7f069c43c595786
minSW <minsw08@gmail.com>
2019-06-06 22:00:54 +0900

    Apply the default theme as monokai

--- resource/home.js
+++ resource/home.js
@@ -14,9 +14,9 @@ _gaq.push(['_trackPageview']);
 function updateCode() {
   $('.code').text("export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'"
                   +"\n--color=fg:" + $('#fg').val() + ",bg:" + $('#bg').val() + ",hl:" + $('#hl').val()
-                  +"fg+:" + $('#fgp').val() + ",bg+:" + $('#bgp').val() + ",hl+:" + $('#hlp').val()
+                  +",fg+:" + $('#fgp').val() + ",bg+:" + $('#bgp').val() + ",hl+:" + $('#hlp').val()
                   +"\n--color=info:" + $('#info').val() + ",prompt:" + $('#prompt').val() + ",pointer:" + $('#pointer').val()
-                  +"marker:" + $('#marker').val() + ",spinner:" + $('#spinner').val() + ",header:" + $('#header').val()
+                  +",marker:" + $('#marker').val() + ",spinner:" + $('#spinner').val() + ",header:" + $('#header').val()
                   +"\n'");
   $('.code').html($('.code').html().replace(/\n/g,'<br/>'));
   
