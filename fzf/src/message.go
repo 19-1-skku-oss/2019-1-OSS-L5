cfe66655dc4dc3b7fc5edb477926eb5863949066
HongBeenKim <dtb05045@gmail.com>
2019-06-09 23:09:51 +0900

    message.go 에서 message handler 구현
    - GUI에서 apply 버튼을 누르면 message handler에게로 color code 전송
    - message handler는 color code를 파싱해서 .ColorConfig 파일에 저장

--- fzf/src/message.go
+++ fzf/src/message.go
@@ -2,52 +2,32 @@ package fzf
 
 import (
 	"encoding/json"
-	"strconv"
 
 	"github.com/asticode/go-astilectron"
 	"github.com/asticode/go-astilectron-bootstrap"
 	"github.com/asticode/go-astilog"
 	"github.com/pkg/errors"
+	"os"
+	"fmt"
 )
 
 // handleMessages handles messages
 func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload interface{}, err error) {
 	switch m.Name {
-	case "sendNum":
+	case "sendCode":
 		var s string
 		if err := json.Unmarshal(m.Payload, &s); err != nil {
 			astilog.Error(errors.Wrap(err, "json.Unmarshal failed"))
 		}
-		num, err := strconv.ParseFloat(s, 64)
-		if err == nil {
-			numbers = append(numbers,num)
+		var outputFile, _ = os.Create(".ColorConfig")
+		defer outputFile.Close()
+		
+		for i, char := range s {
+			if char == '#' {
+				fmt.Fprint(outputFile, s[i:i + 7], "\n")
+			}
 		}
-	case "sendSymbol":
-		var s string
-		if err := json.Unmarshal(m.Payload, &s); err != nil {
-			astilog.Error(errors.Wrap(err, "json.Unmarshal failed"))
-		}
-		if err == nil {
-			symbols = s
-		}
-	case "clear":
-		numbers = numbers[:0]
-		symbols = ""
-	case "result":
-		switch symbols {
-		case "+":
-			result := numbers[0] + numbers[1]
-			bootstrap.SendMessage(w, "resResult", strconv.FormatFloat(result,'f',6,64))
-		case "-":
-			result := numbers[0] - numbers[1]
-			bootstrap.SendMessage(w, "resResult", strconv.FormatFloat(result,'f',6,64))
-		case "x":
-			result := numbers[0] * numbers[1]
-			bootstrap.SendMessage(w, "resResult", strconv.FormatFloat(result,'f',6,64))
-		case "/":
-			result := numbers[0] / numbers[1]
-			bootstrap.SendMessage(w, "resResult", strconv.FormatFloat(result,'f',6,64))
-		}
+
 	default:
 	}
 	return
